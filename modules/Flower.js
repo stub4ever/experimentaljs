648178dd40082e231d0826fc34595bea6615568f
quoc <bui.vquoc@gmail.com>
2019-09-08 14:59:39 +0700

    Setting up a list of flowers

--- modules/Flower.js
+++ modules/Flower.js
@@ -1,22 +1,21 @@
 // https://github.com/naokiishizuq/myWebPage/blob/master/production/src/modules/vehicle.js
 class Flower {
     // create a p5 sketch as an object "instance"
-    constructor(sketch, x, y, id) {
+    constructor(sketch, x, y) {
         // what do we need to setup a flower
         this.p5 = sketch;
         this.objs = window.store.objs; // store
         this.store = window.store;
-        this.id = id
         this.x = x
         this.y = y
-        
+    }
+
+    // create draw method
+    draw = () => {
         this.p5.fill('white')
-        // set mouse X and Y clicked
-        // set in the middle of the square base on the size of square
         this.p5.square(this.x - 25, this.y  - 25 , 50)
         this.p5.noStroke()
         
-        // the overlayer below will draw on the top of square
         this.p5.fill('red')
         this.p5.circle(this.x, this.y, 10)
     }
--- pages/maths/2-flower-brush.vue
+++ pages/maths/2-flower-brush.vue
@@ -1,61 +1,65 @@
 <template>
-    <section>
-        <!-- Here comes drawing area -->
-        <div ref="canvas"></div>
-    </section>
+<section>
+    <!-- Here comes drawing area -->
+    <div ref="canvas"></div>
+</section>
 </template>
 
 <script>
-    import Flower from '@/modules/Flower.js';
+import Flower from '@/modules/Flower.js';
 
-    export default {
-        data() {
-            return {
-                canvas: {
-                    p5: null,
-                    created: null,
-                    script: null,
-                    el: null,
+export default {
+    data() {
+        return {
+            canvas: {
+                p5: null,
+                created: null,
+                script: null,
+                el: null,
+            },
+            flowers: [],
+        }
+    },
+    mounted() {
+        this.initCanvas()
+    },
+    methods: {
+        initCanvas() {
+            this.canvas.el = this.$refs.canvas
+            const P5 = require("p5");
+
+            this.canvas.script = (sketch) => {
+                window.store = {}; // to communicate between vuejs and p5js
+                window.store.objs = []; // list of all objects on canvas
+                let _flower = {}; // Flower interactions
+
+                sketch.setup = () => {
+                    this.canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight).parent(this.canvas.el)
+                    this.canvas.background('#000');
+                }
+
+                sketch.draw = () => {
+                    // Add draw for each flower items to a canvas
+                    this.flowers.forEach(flower => {
+                        flower.draw() 
+                    });
+                }
+
+                sketch.mouseClicked = () => {
+                    // When mouse is clicked push the flower item inside this.flowers array
+                    this.flowers.push(_flower = new Flower(sketch, sketch.mouseX, sketch.mouseY))
                 }
             }
-        },
-        mounted () {
-            this.initCanvas()
-        },
-        methods: {
-            initCanvas() {
-                this.canvas.el = this.$refs.canvas
-                const P5 = require("p5");
 
-                this.canvas.script = (sketch) => {
-                    window.store = {};// to communicate between vuejs and p5js
-                    window.store.objs = [];// list of all objects on canvas
-                    let _FI = {}; // Flower interactions
-                    
-                    sketch.setup = () => {
-                        this.canvas = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight).parent(this.canvas.el)
-                        this.canvas.background('#000'); 
-                    }
-                                        
-                    sketch.draw = () => {
-
-                    }
-
-                    sketch.mouseClicked = () => {
-                        _FI = new Flower(sketch, sketch.mouseX, sketch.mouseY)
-                    }
-                }
-                
-                this.canvas.p5 = new P5( this.canvas.script)
-            },
+            this.canvas.p5 = new P5(this.canvas.script)
         },
-    }
+    },
+}
 </script>
 
 <style lang="scss" scoped>
-   // Prevent no space between gap of the page
-    canvas {
-        vertical-align: bottom;
-    }
-
+// Prevent no space between gap of the page
+canvas {
+    vertical-align: bottom;
+}
 </style>
