059cede5576a3ae28a075bad78f16027a3f1b9f9
quoc <bui.vquoc@gmail.com>
2019-09-11 08:30:05 +0700

    Grouped rotations

--- modules/motions/2-Square.js
+++ modules/motions/2-Square.js
@@ -1,7 +1,8 @@
 class Square {
     // create a two sketch as an object "instance"
-    constructor(two, x, y, width, height, angle) {
+    constructor(two, group, x, y, width, height, angle) {
         this.canvas = two;
+        this.group = group;
         this.x = x;
         this.y = y;
         this.width = width;
@@ -12,12 +13,16 @@ class Square {
     }
 
     init = () => {
+        this.makeRectangle();
+        
+        // add to group
+        this.group.add(this.shape)
+    }
+
+    makeRectangle = () => {
         this.shape = this.canvas.makeRectangle(this.x, this.y, this.width, this.height, this.angle) 
         this.shape.noStroke()
         this.shape.fill = "#f9bc31"
-
-        // Apply angle rotation
-        // this.shape.rotation = this.angle
     }
 
     // We are calling the rotation method from the parent
--- pages/motions/2-grouped-rotations.vue
+++ pages/motions/2-grouped-rotations.vue
@@ -29,7 +29,7 @@ export default {
     data() {
         return {
             canvas: null,
-
+            groupSquares: null,
             square: {
                 numberOfShapes: 12,
                 shapes: [],
@@ -61,6 +61,11 @@ export default {
                 height: 500
             }
             this.canvas = new Two(params).appendTo(this.$refs.canvas)
+            
+            // Groups can take an array of shapes and/or groups it all together
+            this.groupSquares = this.canvas.makeGroup()
+            this.groupSquares.translation.set((params.width / 2) , (params.width / 2)) // set turn arround the middle of canvas
+
 
             for (let index = 0; index < this.square.numberOfShapes; index = index + 1) {
                 // Define the angle
@@ -68,14 +73,14 @@ export default {
                 this.square.angle = fullRotation * index / this.square.numberOfShapes
                 
                 // Define cos direction => radius * cos(angle)
-                this.square.x = (params.width / 2) + this.square.plotRadius * Math.cos(this.square.angle) 
+                this.square.x = this.square.plotRadius * Math.cos(this.square.angle) // + (params.width / 2) => remove this since we correct the transation on the group
                 
                 // Define sin angle => radius * sin(angle)
-                this.square.y = (params.width / 2) + this.square.plotRadius * Math.sin(this.square.angle)
+                this.square.y = this.square.plotRadius * Math.sin(this.square.angle) // + (params.width / 2) => remove this since we correct the transation on the group
 
                 // 1. Add state angle to each individual square
                 // 2. Push every object inside the array => to have the access outside this loop
-                this.square.shapes.push(this.square.el = new Square(this.canvas, this.square.x, this.square.y, this.square.width, this.square.height, this.square.angle))
+                this.square.shapes.push(this.square.el = new Square(this.canvas, this.groupSquares, this.square.x, this.square.y, this.square.width, this.square.height, this.square.angle))
                 
                 // Play each square element
                 this.square.el.play()
@@ -83,7 +88,11 @@ export default {
 
             // bind two.bind(event, callback);
             // Bind an event, string, to a callback function. Passing "all" will bind the callback to all events.
-            this.canvas.bind("update", () => {
+            this.canvas.bind("update", (frameCount) => {
+
+                // Apply updates on the group
+                this.groupSquares.rotation += 0.005
+
                 // 1. Everytime when it update the frame change each element in rotation 
                 // 2. This approach allow to apply outside the scope
                 this.square.shapes.forEach((shape) => {
@@ -92,12 +101,7 @@ export default {
             });
 
 
-            // this.canvas.bind('update', test())
-
-            // function test() {
-            //     console.log('test')
-            // }
-
+            this.canvas.play()
         }
     }
 }
