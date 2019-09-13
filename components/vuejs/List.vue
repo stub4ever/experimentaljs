074131a156186418ca17446731aba8472db5eb9e
quoc <bui.vquoc@gmail.com>
2019-09-13 19:58:32 +0700

    Passing Props and Iterating a Collection

---
+++ components/vuejs/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+        <li v-for="item in listItems" :key="item">
+            <!-- inside the brackets we can apply variable + expression -->
+            {{ item }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    // Pass data from outside to a component through props   
+    props: {
+        listItems: {
+            type: Array,
+            default: []
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/vuejs/1-the-list-of-items.vue
+++ pages/vuejs/1-the-list-of-items.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-
+        
+        <!-- Add component List -->
+        <!-- Pass data attribute by defined a prop ListItems apply kebab case to match with the prop -->
+        <List :list-items="['apple','oranges']" />
     </div>
 </template>
 
 <script>
+    import List from '~/components/vuejs/List'
+
     export default {
-        
+        // Registering component property
+        components: { List }
     }
 </script>
 
