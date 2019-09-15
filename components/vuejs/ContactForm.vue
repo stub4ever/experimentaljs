684dacca3856f3dd40c08782bb72a9d15a65e804
quoc <bui.vquoc@gmail.com>
2019-09-15 13:51:23 +0700

    Binding Form Controls to Compont Data

--- components/vuejs/ContactForm.vue
+++ components/vuejs/ContactForm.vue
@@ -5,9 +5,13 @@
         <fieldset class="flex flex-gap-sm">
             <div class="col-12">
                 <label for="sender-email" class="form-label margin-bottom-xs">Email</label>
+
+                <!-- With v-model we bind data properties with the form control 
+                    Its a two-way binding, when the property inside form controle or any data we specify change it will update auto -->
                 <input 
                     type="email"
                     class="form-control width-100%"
+                    v-model="email"
                 >
             </div>
             
@@ -17,14 +21,17 @@
                     id="sender-message" 
                     rows="10"
                     class="form-control width-100%"
+                    v-model="message"
                 ></textarea>
             </div>
         </fieldset> 
         
         <!-- Add event onclick vue specify directive v-on (shortcut @) + add modifier prevent on click (modifier is chainable) -->
+        <!-- If email or message isn't created disable the button -->
         <button 
             class="btn btn--primary margin-top-sm"
             @click.prevent="submitForm"
+            :disabled="!email || !message"
         >Send message</button>
     </form>
 </template>
@@ -32,9 +39,20 @@
 <script>
     export default {
         name: 'ContactForm',
+        
+        // Defined Data factory that returns a object
+        data() {
+            return {
+                email: '',
+                message: ''
+            }
+        },
         methods: {
             submitForm(event) {
-                console.log(event.target)
+                // validate form
+
+                // submit form
+                console.log('Thank you we will contact you soon')
             }
         },
     }
