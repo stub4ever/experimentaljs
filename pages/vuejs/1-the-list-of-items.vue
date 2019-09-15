5c818bfec2525cf6829807616e0ef5c30ac9dc23
quoc <bui.vquoc@gmail.com>
2019-09-15 11:40:02 +0700

    Using external data

--- pages/vuejs/1-the-list-of-items.vue
+++ pages/vuejs/1-the-list-of-items.vue
@@ -11,17 +11,45 @@
         
         <List 
             item-type='Instrument' 
-            :list-items="['guitar','piano']" 
+            :list-items="instruments" 
         />
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     import List from '~/components/vuejs/List'
 
     export default {
         // Registering component property
-        components: { List }
+        components: { List },
+        
+        // A factory function to return our data object inside the component
+        data() {
+            return {
+                instruments: []
+            }
+        },
+
+        // When component reaches a certain parts of lifecycle, its going to execute a method 
+        // It will auto update the DOM with any change to the data
+
+        // Created() executes whenever the component is just created. Its not on the DOM or Data, its just created
+        created () {
+            // For HTTP request we apply inside created method, because when nothings is in the DOM yet, 
+            // by the time it is in the DOM, we have data back from the server, we might don't have any flickering or anything like that, if yes we can issued the request
+
+            axios.get('/instruments.json').then(res => {
+                // when we get data response set value to our data object instruments
+                this.instruments = res.data.items
+            })
+        },
+        // Mounted executes whenever the component is mounted in the DOM. By the time the mounthed method is executes, it exists in the DOM
+        mounted() {
+
+        }
+
+
     }
 </script>
 
