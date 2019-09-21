ef8399328701840ab0cfa91ce1667a8879997fa5
quoc <bui.vquoc@gmail.com>
2019-09-21 11:14:28 +0700

    Extending components

---
+++ components/vuejs/envato/ConfirmModal.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- Extend component for modal -->
+
+    <!-- Add emit close to hide modal -->
+    <base-modal 
+        :show="show"
+        @close="$emit('close')"
+    >   
+        <!-- Inside template header add a placeholder header slot so that it can float to modal component -->
+        <template slot="header">
+            <slot name="header"></slot>
+        </template>
+
+        <!-- The content goes to default slot inside the Modal component body -->
+        <slot></slot>
+
+        <!-- Use template to prevent extra div wrapping as slot -->
+        <template slot="footer">
+            <div class="flex justify-end flex-gap-xs">
+                <button 
+                    @click.prevent="$emit('close')"
+                    class="reset modal__close-btn" 
+                >Cancel</button>
+
+                <!-- Add emit confirm && close -->
+                <button 
+                    @click.prevent="$emit('close'), $emit('confirm')"
+                    class="reset modal__close-btn" 
+                >Ok</button>
+            </div>
+        </template>
+    </base-modal>
+</template>
+
+<script>
+import BaseModal from '~/components/vuejs/envato/BaseModal'
+
+export default {
+    props: ['show'],
+    components: { BaseModal },
+}
+</script>
---
+++ pages/vuejs/6-extending-components.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+
+    <!-- Add a button to trigger modal -->
+    <!-- add click event listener if click showModal is true -->
+    <button 
+        @click.prevent="showModal" 
+        class="btn btn--primary" 
+        aria-controls="modal1"
+    >Show modal</button>
+    
+    <!-- Use event confirm to emit or v-model(when true or false do something) -->
+    <confirm-modal 
+        :show="isModalVisible" 
+        @close="closeModal"
+        @confirm="DeleteRecource"
+    >   
+        <!-- Use template to prevent extra div wrapping as slot -->
+        <template slot="header">Delete Resource</template>
+
+        <!-- The content goes to default slot inside Modal body -->
+        <p>Are you sure you want to delete</p>
+        
+    </confirm-modal>
+</div>
+</template>
+
+<script>
+import ConfirmModal from '~/components/vuejs/envato/ConfirmModal'
+
+export default {
+    components: {
+        ConfirmModal,
+    },
+    data() {
+        return {
+            isModalVisible: false,
+        }
+    },
+    methods: {
+        showModal() {
+            this.isModalVisible = true;
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        },
+        DeleteRecource() {
+            alert('Resource is deleted')
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
