d7e69e5819d820eb0ff61a22abc15bee24a4a21d
quoc <bui.vquoc@gmail.com>
2019-09-26 22:03:58 +0700

    Creating a Data Provider Component

---
+++ components/vuejs/envato/Http.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios';
+export default {
+    props: {
+        url: {
+            type: String,
+            default: ''
+        },
+    },
+    data() {
+        return {
+            response: {} // empty object
+        };
+    },
+    // Apply the request as soon possible  
+    created() {
+        // Make a request from url then make a response and assign data to property response
+        axios.get(this.url).then(res => this.response = res.data);
+    },
+    render() {
+        // Return a scopedSlot default Vnode with a agrument object (accept data, methods etc)
+        return this.$scopedSlots.default({
+            data: this.response
+        });
+    }
+}
+</script>
--- pages/vuejs/10-creating-a-data-provider-component.vue
+++ pages/vuejs/10-creating-a-data-provider-component.vue
@@ -1,33 +1,40 @@
 <template>
 <div id="app">
 
-    <!-- Passed an arrays of object -->
-    <base-list :items="guitars">
-        <!-- Apply scope slot inside item(li) with object decorative -->
-        <!-- Scoped slots make it possible to pass data from a child component to its parent, 
-        thus allowing complete customization of the content. -->
-        <a :href="`/guitars/${guitar.id}`" slot-scope="{item: guitar, index}">
-            {{guitar.name}}
-        </a>
-    </base-list>
+    <!-- Wrap a utillities component -->
+    <!-- Retrieve json file -->
+    <Http url="/instruments.json">
+
+        <!-- Passed an arrays of object from data HTTP -->
+        <base-list  slot-scope="{data}" :items="data.guitars">
+            <!-- Apply scope slot inside item(li) with object decorative -->
+            <!-- Scoped slots make it possible to pass data from a child component to its parent, 
+            thus allowing complete customization of the content. -->
+            <a :href="`/guitars/${guitar.id}`" slot-scope="{item: guitar, index}">
+                {{guitar.name}}
+            </a>
+        </base-list>
+
+    </Http>
 </div>
 </template>
 
 <script>
 import BaseList from '~/components/vuejs/envato/BaseList'
+import Http from '~/components/vuejs/envato/Http'
 
 export default {
-    components: { BaseList },
+    components: { BaseList, Http },
     name: 'app',
-    data() {
-        return {
-            guitars: [
-                { id: 0, name: 'Les Paul' },
-                { id: 1, name: 'Vela' },
-                { id: 2, name: 'Starla' }
-            ]
-        };
-    }
+    // data() {
+    //     return {
+    //         guitars: [
+    //             { id: 0, name: 'Les Paul' },
+    //             { id: 1, name: 'Vela' },
+    //             { id: 2, name: 'Starla' }
+    //         ]
+    //     };
+    // }
 }
 </script>
 
